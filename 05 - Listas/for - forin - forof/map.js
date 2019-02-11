df50f06f7b332bf42a6ae45f24acaa62349951da
DanielSuhett <danysuhett@hotmail.com>
2019-02-11 18:22:55 -0200

    Array.Map

---
+++ 05 - Listas/for - forin - forof/map.js
@@ -0,0 +1,30 @@
+const service = require('./service')
+
+Array.prototype.meuMap = function(callback) {
+    const novoArrayMapeado = []
+    for (let index = 0; index <= this.length - 1; index++) {
+       const resultado = callback(this[index], index)
+       novoArrayMapeado.push(resultado)
+  }
+    return novoArrayMapeado;
+}
+
+async function main() {
+    try {
+        const result = await service.obterPessoas('a')
+        
+       /*result.results.forEach(item => {
+           names.push(item.name)
+       });*/
+
+       const names = result.results.meuMap(function(pessoa, index) {
+           return `[${index}]${pessoa.name}`
+       })
+
+        console.log(names)
+    } catch (error) {
+        console.log('Nem cheguei a varrer a API bro')
+    }
+}
+
+main()

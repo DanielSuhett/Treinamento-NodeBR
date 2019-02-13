b1e25a71b851ac4b7e65fd19b4c7eae9b2bf69c5
DanielSuhett <danysuhett@hotmail.com>
2019-02-13 11:32:25 -0200

    Filter e ajustes

--- 05 - Listas/for - forin - forof/map.js
+++ 05 - Listas/for - forin - forof/map.js
@@ -1,25 +1,25 @@
 const service = require('./service')
 
-Array.prototype.meuMap = function(callback) {
+Array.prototype.meuMap = function (callback) {
     const novoArrayMapeado = []
     for (let index = 0; index <= this.length - 1; index++) {
-       const resultado = callback(this[index], index)
-       novoArrayMapeado.push(resultado)
-  }
+        const resultado = callback(this[index], index)
+        novoArrayMapeado.push(resultado)
+    }
     return novoArrayMapeado;
 }
 
 async function main() {
     try {
         const result = await service.obterPessoas('a')
-        
-       /*result.results.forEach(item => {
-           names.push(item.name)
-       });*/
 
-       const names = result.results.meuMap(function(pessoa, index) {
-           return `[${index}]${pessoa.name}`
-       })
+        /*result.results.forEach(item => {
+            names.push(item.name)
+        });*/
+
+        const names = result.results.meuMap(function (pessoa, index) {
+            return `[${index}]${pessoa.name}`
+        })
 
         console.log(names)
     } catch (error) {

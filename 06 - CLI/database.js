740ac721c72977f1fa4b9f5bf81397a4494487a1
DanielSuhett <danysuhett@hotmail.com>
2019-02-20 20:26:21 -0300

    commander crud

--- 06 - CLI/database.js
+++ 06 - CLI/database.js
@@ -1,31 +1,90 @@
 const {
-    readFile
-} = require('fs')
+    readFile,
+    writeFile,
+} = require("fs");
 
 const {
     promisify
-} = require('util')
+} = require("util");
 
-const readFileAsync = promisify(readFile)
+const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
 
 class Database {
     constructor() {
-        this.NOME_ARQUIVO = 'herois.json'
+        this.NOME_ARQUIVO = "herois.json"
     }
     async obterDadosArquivo() {
-        const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8')
+        const arquivo = await readFileAsync(this.NOME_ARQUIVO, "utf8");
         return JSON.parse(arquivo.toString())
     }
 
-    escreverArquivo() {
-
+    async escreverArquivo(dados) {
+        await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados))
+        return true
     }
+
+    async cadastrar(heroi) {
+        const dados = await this.obterDadosArquivo()
+
+        const id = heroi.id <= 2 ? heroi.id : Date.now();
+
+        const heroiComID = {
+            id,
+            ...heroi
+        }
+
+        const dadosFinal = [
+            ...dados,
+            heroiComID
+        ]
+        const resultado = await this.escreverArquivo(dadosFinal)
+        return resultado
+    }
+
 
     async listar(id) {
         const dados = await this.obterDadosArquivo()
-        const dadosFiltrados = dados.filter(item => (id ? (item.id === id) : true))
+        const dadosFiltrados = dados.filter(item => (id ? item.id === id : true));
         return dadosFiltrados
+    }
+
+    async remover(id) {
+        if (!id) {
+            return await this.escreverArquivo([])
+        }
+
+        const dados = await this.obterDadosArquivo()
+        const indice = dados.findIndex(item => item.id === parseInt(id))
+
+        //if (indice === -1) {
+        //    throw Error('O heroi informado nem existe')
+        //}
+        dados.splice(indice, 1)
+        return await this.escreverArquivo(dados)
+    }
+
+    async atualizar(id, modificacoes) {
+        const dados = await this.obterDadosArquivo()
+        const indice = dados.findIndex(item => item.id === parseInt(id))
+        if (indice === -1) {
+            throw Error('O heroi informado nem existe')
+        }
+
+        const atual = dados[indice]
+        const objetoAtualizar = {
+            ...atual,
+            ...modificacoes
+        }
+
+        dados.splice(indice, 1)
+
+        return await this.escreverArquivo([
+            ...dados,
+            ...objetoAtualizar
+        ])
+
     }
 }
 
-module.exports = new Database()
+module.exports = new Database();
---
+++ 06 - CLI/index.js
@@ -0,0 +1,68 @@
+const database = require('./database')
+const Commander = require('commander')
+const Heroi = require('./heroi');
+
+async function main() {
+    Commander
+        .version('1.0.0')
+
+        .option('-n, --nome [value]', "Nome do Heroi")
+        .option('-p, --poder [value]', "Poder do Heroi")
+        .option('-i --id [value]', "ID do Heroi")
+
+        .option('-c, --cadastrar', "Cadastrar um heroi")
+        .option('-l, --listar', "Lista herois")
+        .option('-r, --remover', "Remove heroi por ID")
+
+        .parse(process.argv)
+
+    const heroi = new Heroi(Commander)
+
+    try {
+        if (Commander.cadastrar) {
+
+            const resultado = await database.cadastrar(heroi)
+
+            if (resultado === -1) {
+                console.error('\nO herói não foi cadastrado!')
+                return;
+            }
+            console.log('\nO herói foi cadastrado com sucesso')
+        }
+        if (Commander.listar) {
+            const resultado = await database.listar()
+            if (resultado.length <= 0) {
+                console.log('\n NÃO EXISTEM HERÓIS AQUI')
+            } else {
+                console.log("\n                 LISTA DE HERÓIS CADASTRADOS     ")
+            }
+
+            for (let index = 0; index < resultado.length; index++) {
+                console.log(`
+                ----------------------------
+                    Nome: ${resultado[index].nome} 
+
+                    Poder: ${resultado[index].poder}
+
+                    ID: ${resultado[index].id}
+                ----------------------------
+                `)
+            }
+            return;
+        }
+        if (Commander.remover) {
+            const resultado = await database.remover(heroi.id)
+            if (!resultado) {
+                console.error('\nNão foi possivel remover o herói')
+                return;
+            }
+            console.log('\nO herói foi removido com sucesso');
+
+        }
+
+    } catch (error) {
+        console.error('\nDeu xabu\n', error)
+    }
+}
+
+main()

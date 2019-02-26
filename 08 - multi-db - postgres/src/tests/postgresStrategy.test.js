d4fe8f12741c1a6e1ffd1f15e7175f31aeb55073
DanielSuhett <danysuhett@hotmail.com>
2019-02-26 18:26:08 -0300

    Method Create Postgres

---
+++ 08 - multi-db - postgres/src/tests/postgresStrategy.test.js
@@ -0,0 +1,27 @@
+const assert = require('assert')
+const Postgres = require('./../db/strategies/postgres');
+const Context = require('../db/strategies/base/ContextStrategy')
+
+
+const context = new Context(new Postgres())
+const MOCK_HEROI_CADASTRAR = {
+    nome: 'Gaviao Negro',
+    poder: 'flexas'
+}
+
+
+describe('Postgres Strategy', function () {
+    this.timeout(Infinity)
+    this.beforeAll(async function () {
+        await context.connected()
+    })
+    it('PostgresSQL Connection', async function () {
+        const resultado = await context.isConnected();
+        assert.equal(resultado, true)
+    })
+    it('Cadastrar', async function () {
+        const result = await context.create(MOCK_HEROI_CADASTRAR)
+        delete result.id
+        assert.deepEqual(result, MOCK_HEROI_CADASTRAR)
+    })
+})
